45a657a40e285b560f145e13772bb02df07c1bd8
Muhammad Pendar Bintang Kasdiono <muhammad.pendar@ui.ac.id>
2025-02-21 09:42:15 +0700

    first init

---
+++ kamis-fe/src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { RouterView } from 'vue-router'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-screen">
+    <h1 class="text-6xl font-bold text-black">Aku 
+      <span class="text-green-600">KAMIS</span>
+    </h1>
+  </div>
+</template>
