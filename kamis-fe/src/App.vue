e337cee67cb7848aafece34b1b182c5dd4a0ecb7
Fadeeaal <rakha.fadil@ui.ac.id>
2025-03-18 08:58:38 +0700

    [ADD] LeftBar and Icon

--- kamis-fe/src/App.vue
+++ kamis-fe/src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
+import LeftBar from './components/LeftBar.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="app-layout">
+    <LeftBar />
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.app-layout {
+  display: flex;
+  flex-direction: row;
+}
+</style>
