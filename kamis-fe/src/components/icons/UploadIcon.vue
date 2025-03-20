542295bd83b3a47d9d0e56eab8518c8de2cbecbc
Fadeeaal <rakha.fadil@ui.ac.id>
2025-03-20 15:15:35 +0700

    [UPDATE] Update design & Tombol submit

--- kamis-fe/src/components/icons/UploadIcon.vue
+++ kamis-fe/src/components/icons/UploadIcon.vue
@@ -1,17 +1,15 @@
 <template>
-    <font-awesome-icon icon="fa-solid fa-upload" />
-  </template>
-  
-  <script setup lang="ts">
-  // Ikon sudah global dari main.ts, gak perlu import ulang
-  </script>
-  
-  <style scoped>
-  font-awesome-icon {
-    width: 48px;
-    height: 48px;
-    margin-bottom: 8px;
-    color: #1E3A5F;
-  }
-  </style>
-  
+  <font-awesome-icon icon="fa-solid fa-upload" />
+</template>
+
+<script setup lang="ts">
+// Ikon sudah global via main.ts
+</script>
+
+<style scoped>
+font-awesome-icon {
+  width: 48px;
+  height: 48px;
+  color: #1E3A5F;
+}
+</style>
