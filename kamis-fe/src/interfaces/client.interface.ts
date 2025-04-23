d805a13f0199c7e2edd5b66782c8bdde9b397fd4
wirr <75379277+PrawiraRzkN@users.noreply.github.com>
2025-04-23 18:43:11 +0700

    pbi-20: set up (#2)

---
+++ kamis-fe/src/interfaces/client.interface.ts
@@ -0,0 +1,21 @@
+export interface ClientInterface {
+    id: string;
+    nameClient: string;
+    noTelpClient: string;
+    emailClient: string;
+    typeClient: string;
+    companyClient: string;
+    addressClient: string;
+    createdDate: string; // ISO string format (YYYY-MM-DDTHH:MM:SSZ)
+    updatedDate: string; // ISO string format (YYYY-MM-DDTHH:MM:SSZ)
+  }
+
+  export interface AddClientRequestInterface {
+    nameClient: string;
+    noTelpClient: string;
+    emailClient: string;
+    typeClient: boolean; // true = Perusahaan, false = Individu (misalnya)
+    companyClient?: string;
+    addressClient?: string;
+  }
+  
