96dc41c2eb74d0b0861eef029255bb9c7e1dbd8c
Natanael Bonaparte <tastyfuture@gmail.com>
2025-04-27 22:44:32 +0700

    memperbaiki hilang kode pada tabel maintenance

--- kamis-fe/src/views/asset/DetailAssetView.vue
+++ kamis-fe/src/views/asset/DetailAssetView.vue
@@ -148,6 +148,9 @@
                     label="Selesai"
                     @click="completeMaintenance(item.id)"
                   />
+                  <span v-else>
+                    {{ calculateMaintenanceDuration(item.tanggalMulaiMaintenance, item.tanggalSelesaiMaintenance) }} hari
+                  </span>
                 </td>
               </tr>
               <tr v-if="maintenanceHistory.length === 0">
@@ -302,6 +305,19 @@ const sortedMaintenanceHistory = computed(() => {
     return dateB.getTime() - dateA.getTime();
   });
 });
+
+// Calculate maintenance duration in days
+const calculateMaintenanceDuration = (startDate: string, endDate: string): string | number => {
+  if (!startDate || !endDate) return 0;
+  
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const differenceInTime = end.getTime() - start.getTime();
+  const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+  
+  // Return "<1" if the duration is less than 1 day
+  return differenceInDays < 1 ? "<1" : differenceInDays;
+};
 
 // Fetch maintenance data
 const fetchMaintenanceHistory = async () => {
